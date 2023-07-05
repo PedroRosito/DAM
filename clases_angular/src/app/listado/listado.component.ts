b543f70003ed256687f3517a89e104a973e498fa
pedro <rositopedro@gmail.com>
2023-07-05 17:01:49 -0300

    Se utilizan directivas, servicios y pipe

--- clases_angular/src/app/listado/listado.component.ts
+++ clases_angular/src/app/listado/listado.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoggerService } from '../services/logger.service';
+import { ListadoDispositivoService } from '../services/listado-dispositivo.service';
+import { Dispositivo } from '../interfaces/dispositivo';
 
 @Component({
   selector: 'app-listado',
@@ -7,12 +10,21 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
+  constructor(
+    private loggerService: LoggerService,
+    private listadoDispositivoService: ListadoDispositivoService
+  ) {}
+
   texto: string = 'Hola mundo Angular'
   buttonState: boolean = false
   numeroTarjeta: string = ''
+  number: number = 2
+
+  listado: Dispositivo[] = this.listadoDispositivoService.getListadoDispositivos()
 
   changeState (string: string) {
     this.buttonState = !this.buttonState
-    console.log(string)
+    this.loggerService.logWarning(string)
+    this.loggerService.log(this.listadoDispositivoService.getListadoDispositivos())
   }
 }
