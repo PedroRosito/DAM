427e2dab997fdac8546b75db58b03e9d6198987a
pedro <rositopedro@gmail.com>
2023-07-19 18:56:32 -0300

    Se añade app fullstack con ionic como frontend

---
+++ app-fullstack-base-2022-i07/src/backend/index.js
@@ -0,0 +1,43 @@
+//=======[ Settings, Imports & Data ]==========================================
+
+var PORT    = 3000;
+
+const cors = require('cors');
+
+var express = require('express');
+var app     = express();
+var pool   = require('./mysql-connector');
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// to parse application/json
+app.use(express.json()); 
+// to serve static files
+app.use(express.static('/home/node/app/static/'));
+// to enable cors
+app.use(cors(corsOptions));
+
+//=======[ Main module code ]==================================================
+
+app.get('/', function(req, res, next) {
+    res.send({'mensaje': 'Hola DAM'}).status(200);
+});
+
+app.get('/devices/', function(req, res, next) {
+    pool.query('Select * from Dispositivos', function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.listen(PORT, function(req, res) {
+    console.log("NodeJS API running correctly");
+});
+
+//=======[ End of file ]=======================================================
