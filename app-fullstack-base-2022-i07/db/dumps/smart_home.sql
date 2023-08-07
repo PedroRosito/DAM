594ba7eae667b14777a5bf1782e1a68a8674b009
pedro <rositopedro@gmail.com>
2023-08-07 19:46:01 -0300

    Se añade nuevo dump para base de datos de DAM

--- app-fullstack-base-2022-i07/db/dumps/smart_home.sql
+++ app-fullstack-base-2022-i07/db/dumps/smart_home.sql
@@ -1,15 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 26, 2019 at 02:50 PM
--- Server version: 5.7.26-0ubuntu0.16.04.1
--- PHP Version: 7.0.33-0ubuntu0.16.04.4
-
-CREATE DATABASE IF NOT EXISTS smart_home;
-
-USE smart_home;
+-- Servidor: mysql-server
+-- Tiempo de generación: 30-11-2020 a las 23:27:10
+-- Versión del servidor: 5.7.27
+-- Versión de PHP: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -23,54 +19,182 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `smart_home`
+-- Base de datos: `DAM`
 --
+CREATE DATABASE IF NOT EXISTS `DAM` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `DAM`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Devices`
+-- Estructura de tabla para la tabla `Dispositivos`
 --
 
-CREATE TABLE `Devices` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `description` varchar(128) NOT NULL,
-  `state` int(11) NOT NULL,
-  `type` int(11) NOT NULL
+CREATE TABLE `Dispositivos` (
+  `dispositivoId` int(11) NOT NULL,
+  `nombre` varchar(200) DEFAULT NULL,
+  `ubicacion` varchar(200) DEFAULT NULL,
+  `electrovalvulaId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Devices`
+-- Volcado de datos para la tabla `Dispositivos`
 --
 
-INSERT INTO `Devices` (`id`, `name`, `description`, `state`, `type`) VALUES
-(1, 'Lampara 1', 'Luz living', 1, 0),
-(2, 'Lampara 2', 'Luz cocina', 0, 0),
-(3, 'Velador', 'Velador living', 1, 0),
-(4, 'Persiana 1', 'Persiana living', 1, 1),
-(5, 'Persiana 2', 'Persiana de la cocina', 1, 1),
-(6, 'Persiana 3', 'Persiana balcon', 0, 1);
+INSERT INTO `Dispositivos` (`dispositivoId`, `nombre`, `ubicacion`, `electrovalvulaId`) VALUES
+(1, 'Sensor 1', 'Patio', 1),
+(2, 'Sensor 2', 'Cocina', 2),
+(3, 'Sensor 3', 'Jardin Delantero', 3),
+(4, 'Sensor 4', 'Living', 4),
+(5, 'Sensor 5', 'Habitacion 1', 5),
+(6, 'Sensor 6', 'Habitacion 2', 6);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `Electrovalvulas`
+--
+
+CREATE TABLE `Electrovalvulas` (
+  `electrovalvulaId` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Electrovalvulas`
+--
+
+INSERT INTO `Electrovalvulas` (`electrovalvulaId`, `nombre`) VALUES
+(1, 'eLPatio'),
+(2, 'eLCocina'),
+(3, 'eLJardinDelantero'),
+(4, 'eLLiving'),
+(5, 'eLHabitacion1'),
+(6, 'eLHabitacion2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Log_Riegos`
+--
+
+CREATE TABLE `Log_Riegos` (
+  `logRiegoId` int(11) NOT NULL,
+  `apertura` tinyint(4) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `electrovalvulaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Mediciones`
+--
+
+CREATE TABLE `Mediciones` (
+  `medicionId` int(11) NOT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `valor` varchar(100) DEFAULT NULL,
+  `dispositivoId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Mediciones`
+--
+
+INSERT INTO `Mediciones` (`medicionId`, `fecha`, `valor`, `dispositivoId`) VALUES
+(1, '2020-11-26 21:19:41', '60', 1),
+(2, '2020-11-26 21:19:41', '40', 1),
+(3, '2020-11-26 21:19:41', '30', 2),
+(4, '2020-11-26 21:19:41', '50', 3),
+(5, '2020-11-26 21:19:41', '33', 5),
+(6, '2020-11-26 21:19:41', '17', 4),
+(7, '2020-11-26 21:19:41', '29', 6),
+(8, '2020-11-26 21:19:41', '20', 1),
+(9, '2020-11-26 21:19:41', '44', 4),
+(10, '2020-11-26 21:19:41', '61', 5),
+(11, '2020-11-26 21:19:41', '12', 2);
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `Devices`
+-- Indices de la tabla `Dispositivos`
 --
-ALTER TABLE `Devices`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `Dispositivos`
+  ADD PRIMARY KEY (`dispositivoId`,`electrovalvulaId`),
+  ADD KEY `fk_Dispositivos_Electrovalvulas1_idx` (`electrovalvulaId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indices de la tabla `Electrovalvulas`
+--
+ALTER TABLE `Electrovalvulas`
+  ADD PRIMARY KEY (`electrovalvulaId`);
+
+--
+-- Indices de la tabla `Log_Riegos`
+--
+ALTER TABLE `Log_Riegos`
+  ADD PRIMARY KEY (`logRiegoId`,`electrovalvulaId`),
+  ADD KEY `fk_Log_Riegos_Electrovalvulas1_idx` (`electrovalvulaId`);
+
+--
+-- Indices de la tabla `Mediciones`
+--
+ALTER TABLE `Mediciones`
+  ADD PRIMARY KEY (`medicionId`,`dispositivoId`),
+  ADD KEY `fk_Mediciones_Dispositivos_idx` (`dispositivoId`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `Devices`
+-- AUTO_INCREMENT de la tabla `Dispositivos`
 --
-ALTER TABLE `Devices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `Dispositivos`
+  MODIFY `dispositivoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `Electrovalvulas`
+--
+ALTER TABLE `Electrovalvulas`
+  MODIFY `electrovalvulaId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `Log_Riegos`
+--
+ALTER TABLE `Log_Riegos`
+  MODIFY `logRiegoId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Mediciones`
+--
+ALTER TABLE `Mediciones`
+  MODIFY `medicionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Dispositivos`
+--
+ALTER TABLE `Dispositivos`
+  ADD CONSTRAINT `fk_Dispositivos_Electrovalvulas1` FOREIGN KEY (`electrovalvulaId`) REFERENCES `Electrovalvulas` (`electrovalvulaId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Log_Riegos`
+--
+ALTER TABLE `Log_Riegos`
+  ADD CONSTRAINT `fk_Log_Riegos_Electrovalvulas1` FOREIGN KEY (`electrovalvulaId`) REFERENCES `Electrovalvulas` (`electrovalvulaId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Mediciones`
+--
+ALTER TABLE `Mediciones`
+  ADD CONSTRAINT `fk_Mediciones_Dispositivos` FOREIGN KEY (`dispositivoId`) REFERENCES `Dispositivos` (`dispositivoId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
