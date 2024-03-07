7e3571812fec18d4412b383e01c1e5a7be9dfed2
pedro <rositopedro@gmail.com>
2024-03-06 22:00:02 -0300

    Se declara elevar pipe, colorear directive y se provee logger service

--- clases_angular/src/app/listado/listado.module.ts
+++ clases_angular/src/app/listado/listado.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListadoComponent } from './listado.component';
 import { FormsModule } from '@angular/forms';
+import { LoggerService } from '../services/logger.service';
+import { ElevarPipe } from '../pipes/elevar.pipe';
+import { ColorearDirective } from '../directives/colorear.directive';
 
 
 @NgModule({
-  declarations: [ListadoComponent],
+  declarations: [ListadoComponent, ElevarPipe, ColorearDirective],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: [ListadoComponent]
+  exports: [ListadoComponent],
+  providers: [LoggerService]
 })
 export class ListadoModule { }
