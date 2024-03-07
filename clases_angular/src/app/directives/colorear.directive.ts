149b96bf28356013f508b873bd9895ab007aca5b
pedro <rositopedro@gmail.com>
2024-03-06 21:59:28 -0300

    Se añade directiva para colorear elemento html

---
+++ clases_angular/src/app/directives/colorear.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appColorear]'
+})
+export class ColorearDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter')
+  mouseEnter () {
+    this.colorear('blue')
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave () {
+    this.colorear('')
+  }
+
+  colorear (color: string) {
+    this.el.nativeElement.style.backgroundColor = color
+  }
+}
